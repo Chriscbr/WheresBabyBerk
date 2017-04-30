b0e7885b75ff29d08f15b6877a45db2db96104db
Chris <crybicki98@gmail.com>
2017-04-30 05:17:51 -0400

    Add TIMES

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
