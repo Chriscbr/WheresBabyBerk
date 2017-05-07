6390969cef704d4a1273ccf71f92a5868a5b0c94
Chris <crybicki98@gmail.com>
2017-05-07 03:42:11 -0400

    Add parsing mechanism that obtains dates from tweets

--- index.js
+++ index.js
@@ -10,15 +10,21 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-var bb1_tweets;
-var bb2_tweets;
-var bb1_data;
-var bb2_data;
+var bb1_tweets, bb2_tweets, bb1_data, bb2_data;
 
 client.get('search/tweets', {q: 'from:UMassBabyBerk'}, function(error, tweets, response) {
+  if (error) {
+    console.log('Error thrown from Twitter API Search query "from:UMassBabyBerk"')
+  }
   bb1_tweets = tweets;
   console.log(tweets);
-  bb1_data = interpret.extract_data(bb1_tweets, bb2_tweets);
+  bb1_data = interpret.extract_data(bb1_tweets);
+});
+
+client.get('search/tweets', {q: 'from:UMassBabyBerk2'}, function(error, tweets, response) {
+  bb2_tweets = tweets;
+  console.log(tweets);
+  bb2_data = interpret.extract_data(bb2_tweets);
 });
 
 app.set('port', (process.env.PORT || 5000));
@@ -32,7 +38,8 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   var data1 = bb1_data;
   var data2 = bb2_data;
-  console.log(data);
+  // console.log(data1);
+  // console.log(data2);
   response.render('pages/index', {
     content1: data1,
     content2: data2
